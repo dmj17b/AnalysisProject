1e7a6665fc0ca716d2d4de3e748c051dd4698655
Joseph Lupton <43359031+jlupdawg@users.noreply.github.com>
2022-11-08 09:33:08 -0500

    Changed Analytic Alg. to not use abs()

--- min_x_func.m
+++ min_x_func.m
@@ -6,7 +6,7 @@ tmp_bounds = bounds;
 search_space = tmp_bounds(1):0.001:tmp_bounds(2);
 
 zero_search = zeros_func(search_space);
-[~,idx] = find(abs(diff(zero_search)) < 0.1,length(search_space));
+[~,idx] = find(diff(zero_search) < 0.1,length(search_space));
 min_v = search_space(idx);
 
 [~, idx] = min(sqrt((min_v - x).^2 + (func(min_v) - y).^2));
